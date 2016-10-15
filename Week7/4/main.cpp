954833c58f7d9967d5fe89a34f85ca1a9e310801
Max Teryokhin <termax1998@gmail.com>
2016-10-15 16:29:16 +0300

    Task # fixed

--- Week7/4/main.cpp
+++ Week7/4/main.cpp
@@ -12,7 +12,7 @@ const int MAX_STRING_LEN = 256;
 
 int main()
 {
-    cout << "Week #7, Task #3" << endl;
+    cout << "Week #7, Task #4" << endl;
     cout << "Input string: ";
     char s[MAX_STRING_LEN], str1[MAX_STRING_LEN/2], str2[MAX_STRING_LEN/2], new_s[MAX_STRING_LEN*2]= {'\0'};
     cin.getline(s, MAX_STRING_LEN);
