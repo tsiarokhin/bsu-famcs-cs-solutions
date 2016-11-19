3f1b0490d1f86c7abafe65ffddee4522ec2952f6
Max Teryokhin <termax1998@gmail.com>
2016-11-19 18:27:25 +0300

    Task #1 solution upload

---
+++ Week10/1/Rational.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "Rational.h"
+
+using namespace std;
+
+void Rational::print()
+{
+    cout << a << '/' << b;
+}
+
+int gcd(int a, int b)
+{
+    while ((a %= b) && (b %= a));
+    return a | b;
+}
+
+void Rational::reduce()
+{
+    int t = gcd(a, b);
+    a /= t;
+    b /= t;
+    if (b < 0){
+        a *= -1;
+        b *= -1;
+    }
+}
+
+Rational Rational::add(const Rational &rhs) const
+{
+    Rational t(a*rhs.b + rhs.a*b, b*rhs.b);
+    t.reduce();
+    return t;
+}
+
+Rational Rational::mul(const Rational &rhs) const
+{
+    Rational t(a*rhs.a, b*rhs.b);
+    t.reduce();
+    return t;
+}
+
+Rational Rational::div(const Rational &rhs) const
+{
+    Rational t(a*rhs.b, b*rhs.a);
+    t.reduce();
+    return t;
+}
+
+bool Rational::equal(Rational &rhs)
+{
+    reduce();
+    rhs.reduce();
+    return (a == rhs.a && b == rhs.b);
+}
+
+bool Rational::greater(Rational &rhs)
+{
+    reduce();
+    rhs.reduce();
+    if (b == rhs.b)
+        return a > rhs.a;
+    else
+        return b < rhs.b;
+}
+
+bool Rational::less(Rational &rhs)
+{
+    reduce();
+    rhs.reduce();
+    if (b == rhs.b)
+        return a < rhs.a;
+    else
+        return b > rhs.b;
+}
