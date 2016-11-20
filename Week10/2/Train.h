2f56379bf97fc5668f913c5fc32948052ce0dd28
Max Teryokhin <termax1998@gmail.com>
2016-11-20 15:04:05 +0300

    Task #2 solution upload

---
+++ Week10/2/Train.h
@@ -0,0 +1,19 @@
+#ifndef TRAIN_H
+#define TRAIN_H
+
+
+class Train
+{
+    public:
+        Train(): dest(0), route(0), express(false) {}
+        Train(int rt, const char* dst, bool expr);
+        Train(const Train& other) {*this = other;}
+        void print() const;
+        bool goesTo(char* dst, bool expr=false) const;
+    private:
+        char* dest;
+        int route;
+        bool express;
+};
+
+#endif // TRAIN_H
