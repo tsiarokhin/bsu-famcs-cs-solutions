7d62845127592dd92514e87a11fb30a8ad3409b7
Max Teryokhin <max.teryokhin@gmail.com>
2017-02-16 18:00:13 +0300

    Fixed #3 and code formatted

--- Term2/Practice/Week01/1/main.cpp
+++ Term2/Practice/Week01/1/main.cpp
@@ -1,31 +1,29 @@
-#include <iostream>
 #include "./vect.h"
+#include <iostream>
 
 int main()
 {
     vector<double> v(5);
-    vector<double>::iterator p ;
+    vector<double>::iterator p;
     int i = 0;
-    for (p = v.begin() ; p != v.end(); ++p)
+    for (p = v.begin(); p != v.end(); ++p)
         *p = 1.5 + i++;
-    do
-    {
+    do {
         --p;
         cout << *p << " , ";
-    }
-    while (p != v.begin());
+    } while (p != v.begin());
     cout << endl;
 
-    vectorex<int>ve(3);
+    vectorex<int> ve(3);
     ve[0] = 1;
     ve[1] = 2;
     ve[2] = 3;
-    for (auto p = ve.begin(); p!=ve.end(); p++)
+    for (auto p = ve.begin(); p != ve.end(); p++)
         cout << *p << ' ';
     cout << endl;
     ve.push_front(123);
     ve.push_front(222);
     ve.push_front(333);
-    for (auto p = ve.begin(); p!=ve.end(); p++)
+    for (auto p = ve.begin(); p != ve.end(); p++)
         cout << *p << ' ';
 }
--- Term2/Practice/Week01/1/vect.h
+++ Term2/Practice/Week01/1/vect.h
@@ -1,19 +1,19 @@
 #ifndef VECT_H_INCLUDED
 #define VECT_H_INCLUDED
-#include  <iostream>
-#include  <assert.h>
+#include <assert.h>
+#include <iostream>
 using namespace std;
 
-template <class T> class vector
-{
+template <class T>
+class vector {
 public:
     typedef T* iterator;
-    explicit vector(int n=100); //create a size n array
+    explicit vector(int n = 100); //create a size n array
     vector(const vector<T>& v); //copy vector
     vector(const T* a, int n); //copy an array
     ~vector()
     {
-        delete []p;
+        delete[] p;
     }
     iterator begin()
     {
@@ -26,14 +26,16 @@ public:
     //T& operator[](int i);      //range checked element
     vector<T>& operator=(const vector<T>& v);
     //protected:
-    T* p;                      //base pointer
-    int size;                  //number of elements
+    T* p; //base pointer
+    int size; //number of elements
 protected:
     int actual_size;
 };
 
 template <class T>
-vector<T>::vector(int n): size(n), actual_size(n)
+vector<T>::vector(int n)
+    : size(n)
+    , actual_size(n)
 {
     assert(n > 0);
     p = new T[actual_size];
@@ -46,7 +48,8 @@ vector<T>::vector(const T a[], int n)
     actual_size = size = n;
     p = new T[actual_size];
     assert(p != 0);
-    for (int i = 0; i < size; ++i)  p[i] = a[i];
+    for (int i = 0; i < size; ++i)
+        p[i] = a[i];
 }
 template <class T>
 vector<T>::vector(const vector<T>& v)
@@ -67,10 +70,11 @@ vector<T>& vector<T>::operator=(const vector<T>& v)
     return *this;
 }
 
-template <class T> class vectorex: public vector<T>
-{
+template <class T>
+class vectorex : public vector<T> {
 public:
-    vectorex(int n = 10):vector<T>::vector(n)
+    vectorex(int n = 10)
+        : vector<T>::vector(n)
     {
         assert(n > 0);
         this->size = n;
@@ -80,29 +84,25 @@ public:
     }
     T& operator[](int i)
     {
-        assert (i >= 0 && i < this->size );
+        assert(i >= 0 && i < this->size);
         return (this->p[i]);
     }
     void push_front(T element)
     {
-        if (this->size + 1 <= this->actual_size)
-        {
+        if (this->size + 1 <= this->actual_size) {
             for (int i = this->size; i > 0; --i)
-                this->p[i] = this->p[i-1];
-        }
-        else
-        {
-            auto temp_p = new T[this->actual_size*2];
+                this->p[i] = this->p[i - 1];
+        } else {
+            this->actual_size *= 2;
+            auto temp_p = new T[this->actual_size];
             for (int i = 0; i < this->size; ++i)
-                temp_p[i+1] = this->p[i];
-            delete []this->p;
+                temp_p[i + 1] = this->p[i];
+            delete[] this->p;
             this->p = temp_p;
         }
         this->p[0] = element;
         this->size++;
     }
 };
-
-
 
 #endif // VECT_H_INCLUDED
